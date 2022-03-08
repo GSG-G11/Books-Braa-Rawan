0f4cb48e0925eb4271cedb4995fb1cc6d6f5ec70
braaAwni <braaberoo95@gmail.com>
2022-03-08 21:37:08 +0200

    database test

--- src/database/config/connection.js
+++ src/database/config/connection.js
@@ -1,10 +1,17 @@
 require('env2')('.env');
 const {Pool} = require('pg');
 
-if(!process.env.DB_URL){
-    throw new Error ('database not found');
-}
+let DB_URL=''
 
+if(process.env.NODE_ENV='development'){
+    DB_URL=process.env.DB_URL_Dev
+}
+else if(process.env.NODE_ENV='test'){
+    DB_URL=process.env.DB_URL_Test
+}
+else if(!process.env.DB_URL){
+    throw new Error ('database not found')
+}
 const connections = new Pool({
     connectionString:process.env.DB_URL,
     ssl : false
---
+++ src/test/database.test.js
@@ -0,0 +1,38 @@
+const connection = require('../database/config/connection');
+const dbBuild = require('../database/config/build');
+
+const delBok = require('../database/query/deleteData');
+const gitAut = require('../database/query/gitAuthors');
+const gitDat = require('../database/query/gitData');
+const postDat = require('../database/query/postData');
+
+beforeEach(()=> dbBuild() );
+afterAll(()=>connection.end());
+
+test('test get data',()=>{
+    return gitDat()
+    .then((data)=>{
+        expect(data.rows.length).toBe(2);
+    })
+});
+test('test get authors',()=>{
+    return gitAut()
+    .then((data)=>{
+        expect(data.rows.length).toBe(2);
+    })
+});
+
+
+test('test post data',()=>{
+    return postDat('ali',22,2)
+    .then((data)=>{
+        expect(data.rows[0].name).toBe('ali');
+    })
+});
+
+test('test delete data',()=>{
+    return delBok(2)
+    .then((data)=>{
+        expect(data.rows.length).toBe(1);
+    })
+});
