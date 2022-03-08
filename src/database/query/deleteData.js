20eda3c91df394cab449ad164f1ecbd97da2a571
braaAwni <braaberoo95@gmail.com>
2022-03-08 21:57:53 +0200

    final testing

--- src/database/query/deleteData.js
+++ src/database/query/deleteData.js
@@ -2,7 +2,7 @@ const connection = require('../config/connection');
 
 const deleteBook=(id)=>{
  const sql ={
-text:'delete from books where id=$1',
+text:'delete from books where id=$1 returning *',
 values:[id]
     }
 return connection.query(sql)
