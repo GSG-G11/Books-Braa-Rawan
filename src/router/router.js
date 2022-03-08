ba55eca2dd299dd2a63ba52cc922c4b37f192135
braaAwni <braaberoo95@gmail.com>
2022-03-08 15:09:06 +0200

    handle error

--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,8 @@
 const express = require('express');
-
+const {handleErrorNotFound,handleErrorServer} = require('../controller/handelError')
 const router = express.Router();
 
+router.use(handleErrorNotFound)
+router.use(handleErrorServer)
 
 module.exports = router;
