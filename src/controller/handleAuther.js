f686b05e150ef4379898547fe588ec5fbc5f1a6d
braaAwni <braaberoo95@gmail.com>
2022-03-09 16:52:51 +0200

    final edit

--- src/controller/handleAuther.js
+++ src/controller/handleAuther.js
@@ -2,7 +2,7 @@ const getAuthor=require('../database/query/gitAuthors')
 
 const allAuthor = (req,res)=>{
     getAuthor()
-    .then(data=>res.json(data.rows)).catch(err=>console.log(err))
+    .then(data=>res.json(data.rows)).catch((err) => res.status(500).json({ msg: 'Internal Server Error', err }))
 }
 
 module.exports=allAuthor;
