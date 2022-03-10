1b4974a837492dfa95bdd7f9ae8af2396fce94d0
braaAwni <91385875+braaAwni@users.noreply.github.com>
2022-03-10 10:26:58 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
+const compression = require('compression');
 const router = require('./router/router');
 
 const app = express();
+app.use(compression());
+app.disable('x-powered-by');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '..', 'public')));
